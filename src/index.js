10cf38e075aa032453414e7f9f4953041ffc4a88
Mixail <misha.senc@mail.ru>
2021-03-20 19:12:31 +0300

    connecting dependencies to the main file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-document.on
+// import "materialize-css";
+import pointsGeneration from "./components/points-generation";
+import createStartPanel from "./components/canvas";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector("#container");
+    const settings = { width: 750, height: 750, cellSize: 7, cellType: "empty" };
+    const colors = { empty: "#D3D3D3", yng: "#008000", yang: "#FF0000", grid: "black" };
+    const { canvas, grid, panel } = createStartPanel(colors, settings);
+
+    pointsGeneration(panel, canvas, settings, colors);
+
+    // document.querySelector("#generate_button").addEventListener("click", () => console.log(panel));
+
+    container.appendChild(canvas);
+    container.appendChild(grid);
+
+    function start(params) {}
+});
