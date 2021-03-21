4da1a4270c22c8a7a6ddc80afa98f23685869951
Mixail <misha.senc@mail.ru>
2021-03-21 15:58:11 +0300

    moved the implementation of the putCoordinate function to the canvas module

--- src/components/points-generation.js
+++ src/components/points-generation.js
@@ -1,9 +1,11 @@
 import EmptyPoints from "./empty-points";
 
-function pointsGeneration(panel, canvas, settings, colors) {
-    const ctx = canvas.getContext("2d");
-    let { width, height, cellSize, cellType } = settings;
+function pointsGeneration(startPanel, colors) {
+    const canvas = startPanel.canvas,
+        ctx = canvas.getContext("2d");
+    let { width, height, cellSize } = startPanel.settings;
 
+    /* -----------------------Рандомная генерация-------------------------------------------------*/
     function randomGeneration() {
         const percentYin = document.querySelector("#yin_percent"),
             percentYang = document.querySelector("#yang_percent"),
@@ -20,89 +22,52 @@ function pointsGeneration(panel, canvas, settings, colors) {
         percentYang.addEventListener("input", () => changePercent(percentYin, percentYang));
         percentYin.addEventListener("input", () => changePercent(percentYang, percentYin));
         countAlive.addEventListener("input", () => {
-            countAlive.value = +countAlive.value < 0 ? 0 : +countAlive.value;
+            let maxValue = Math.floor((width - 1) / cellSize) * Math.floor((height - 1) / cellSize),
+                curValue = +countAlive.value;
+            countAlive.value = curValue < 0 ? 0 : curValue > maxValue ? maxValue : +countAlive.value;
         });
 
-        // const maxX = Math.floor(width),
-        //     maxY = Math.floor(height);
-
-        //TODO написать обработку события рандомной генерации
+        // кнопка генерации точек на полотне
         generButton.addEventListener("click", () => {
             if (!countAlive.value || !percentYin.value || !percentYang.value) {
                 console.log("null check failed!!!");
                 return;
             }
 
-            let countYng = (+countAlive.value * +percentYin.value) / 100,
-                countYang = +countAlive.value - countYng;
+            let countYng = Math.round((+countAlive.value * +percentYin.value) / 100),
+                countYang = +countAlive.value - countYng,
+                maxSizeX = Math.floor((width - 1) / cellSize) * cellSize,
+                maxSizeY = Math.floor((height - 1) / cellSize) * cellSize;
 
-            let { maxSizeX, maxSizeY } = getMaxPointsCount(width - 1, height - 1);
-
-            // Сренерировать масив перемешанных точек на плоскости
-
-            canvas.style.backgroundColor = colors.empty;
+            // Сгенерировать масив рандомно перемешанных точек на плоскости
             const emptyPoints = new EmptyPoints(maxSizeX / cellSize, maxSizeY / cellSize).shuffle();
 
-            cellType = "yng";
+            // очищаем полотно
+            startPanel.clearCanvas();
+            startPanel.setDefaultMatrix();
+
+            // ставим новые точки
+            startPanel.settings.cellType = "yng";
             for (let i = 0; i < countYng; i++) {
-                let [x, y] = emptyPoints.pop().map(item => item * cellSize);
-                putCord({x,y});
+                let [x, y] = emptyPoints.pop().map((item) => item * cellSize);
+                startPanel.putCoordinate({ x, y });
             }
-            cellType = "yang";
+            startPanel.settings.cellType = "yang";
             for (let i = 0; i < countYang; i++) {
-                let [x, y] = emptyPoints.pop().map(item => item * cellSize);
-                putCord({x,y});
+                let [x, y] = emptyPoints.pop().map((item) => item * cellSize);
+                startPanel.putCoordinate({ x, y });
             }
-            // console.log(panel);
         });
     }
 
-    function putCord({ x, y }) {
-        panel[x / cellSize][y / cellSize] = cellType;
-        ctx.fillStyle = colors[`${cellType}`];
-        ctx.fillRect(x, y, cellSize, cellSize);
-    }
-
-    function getUnicRandomCord(maxX, maxY) {
-        let x = Math.floor(maxX / cellSize) * cellSize;
-        let y = Math.floor(maxY / cellSize) * cellSize;
-
-        if (panel[x / cellSize][y / cellSize]) {
-            console.log("uniq");
-            return getUnicRandomCord(maxX, maxY, panel);
-        }
-
-        // console.log(x, y);
-        return {
-            x,
-            y,
-        };
-    }
-
-    function getMaxPointsCount(maxX, maxY) {
-        return {
-            maxSizeX: Math.floor(maxX / cellSize) * cellSize,
-            maxSizeY: Math.floor(maxY / cellSize) * cellSize,
-        };
-    }
-
+    /* -----------------------Пользовательская генерация------------------------------------------*/
     function customGeneration() {
-        const radio = document.querySelectorAll('[name="radio"]'),
-            typeButton = document.querySelector("#input_type_button");
+        const radio = document.querySelectorAll('[name="radio"]');
 
         radio.forEach((item) => {
-            item.addEventListener("change", (event) => (settings.cellType = cellType = event.target.value));
+            item.addEventListener("change", (event) => (startPanel.settings.cellType = event.target.value));
         });
 
-        let typeButtonFlag = true;
-
-        typeButton.addEventListener("click", () => {
-            typeButton.innerText = typeButtonFlag ? "Установка точек мышкой" : "Ввести координаты";
-            typeButtonFlag = !typeButtonFlag;
-        });
-    }
-
-    function putPointsEvents() {
         // Координаты canvas относительно Window
         function windowToCanvas(canvas, x, y) {
             let bbox = canvas.getBoundingClientRect();
@@ -113,8 +78,10 @@ function pointsGeneration(panel, canvas, settings, colors) {
                 y: Math.floor(y / cellSize) * cellSize,
             };
         }
-        
-        canvas.addEventListener("mousedown", (e) => putCord(windowToCanvas(e.target, e.clientX, e.clientY)));
+
+        canvas.addEventListener("mousedown", (e) => {
+            startPanel.putCoordinate(windowToCanvas(e.target, e.clientX, e.clientY));
+        });
 
         const canvasCoordinates = document.querySelector(".canvas_coordinates");
         canvas.addEventListener("mousemove", (e) => {
@@ -122,9 +89,9 @@ function pointsGeneration(panel, canvas, settings, colors) {
             canvasCoordinates.innerHTML = `X: ${x / cellSize}; Y: ${y / cellSize}`;
         });
     }
+    /* -------------------------------------------------------------------------------------------*/
 
     randomGeneration();
     customGeneration();
-    putPointsEvents();
 }
 export default pointsGeneration;
